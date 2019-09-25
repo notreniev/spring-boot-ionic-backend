7856534d095ab9e103ac0b08b89032c0fe981e1b
notreniev <everton.canez@gmail.com>
2019-09-25 16:53:02 -0300

    Criando repository e service para categoria

--- src/main/java/com/nelioalves/cursomc/resources/CatergoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CatergoriaResource.java
@@ -3,33 +3,27 @@ package com.nelioalves.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CatergoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		Categoria cat3 = new Categoria(3, "Portaria");
-		Categoria cat4 = new Categoria(4, "Cafeteria");
-		Categoria cat5 = new Categoria(5, "Banco");
-		Categoria cat6 = new Categoria(6, "RH");
-		
-		List<Categoria> lista = new ArrayList<Categoria>();
-		lista.add(cat1);
-		lista.add(cat2);
-		lista.add(cat3);
-		lista.add(cat4);
-		lista.add(cat5);
-		lista.add(cat6);
-		
-		return lista;
+
+	@Autowired
+	private CategoriaService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
